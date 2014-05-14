a65cf42d15aace53399e7d51e3b4e8e716ac1292
Phil Nguyen <pnguyen06@gmail.com>
2014-05-13 22:46:35 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thestaple::Application.config.secret_key_base = '2913140e051c2895fcdb77e67159a873804c7149d6c2582eeb852f1b473838fdb0148fdccd4b0cc3e7c6917fe3c4b446fcbb8470bce3653716fc146df9b1ae55'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Thestaple::Application.config.session_store :cookie_store, key: '_thestaple_session'
